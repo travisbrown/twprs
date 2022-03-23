f9165ba2707a479f96ac388fb226dde96ed674e1
Travis Brown <travisrobertbrown@gmail.com>
2022-03-23 10:22:39 +0100

    Consistency, etc.

--- src/bin/avro.rs
+++ src/bin/avro.rs
@@ -1,8 +1,9 @@
 use chrono::{Date, NaiveDate, TimeZone, Utc};
 use clap::Parser;
+use flate2::read::GzDecoder;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines, Write};
 use std::path::Path;
 use twprs::model::User;
 
@@ -12,37 +13,70 @@ fn main() -> Result<(), Error> {
     match opts.command {
         Command::Create { input, output } => {
             let path = Path::new(&input);
+
             let output_file = File::create(output)?;
             let mut writer = twprs::avro::writer(output_file);
 
             if path.is_file() {
-                let reader = BufReader::new(File::open(path)?);
-
-                for line in reader.lines() {
-                    let user = serde_json::from_str::<User>(&line?)?;
-                    writer.append_ser(user)?;
-                }
+                write_from_path(path, &mut writer)?;
             } else if path.is_dir() {
                 for entry in std::fs::read_dir(path)? {
-                    let entry = entry?;
-                    eprintln!("Reading file: {:?}", entry.path());
-                    let reader = BufReader::new(File::open(entry.path())?);
-
-                    for (i, line) in reader.lines().enumerate() {
-                        let user = match serde_json::from_str::<User>(&line?) {
-                            Ok(value) => value,
-                            Err(error) => {
-                                panic!("At {}: {:?}", i, error);
-                            }
-                        };
-                        writer.append_ser(user)?;
-                    }
+                    write_from_path(entry?.path(), &mut writer)?;
                 }
+            }
+
+            writer.flush()?;
+        }
+        Command::Dump { input } => {
+            let file = File::open(input)?;
+            let reader = twprs::avro::reader(file)?;
+
+            for value in reader {
+                let user = apache_avro::from_value::<User>(&value?)?;
+
+                println!("{},{}", user.id(), user.snapshot);
             }
         }
     }
 
     Ok(())
+}
+
+fn write_from_path<P: AsRef<Path>, W: Write>(
+    path: P,
+    writer: &mut apache_avro::Writer<W>,
+) -> Result<(), Error> {
+    eprintln!("Reading file: {:?}", path.as_ref().to_string_lossy());
+    let lines = lines(path)?;
+
+    for (i, line) in lines.enumerate() {
+        let user = match serde_json::from_str::<User>(&line?) {
+            Ok(value) => value,
+            Err(error) => {
+                panic!("At {}: {:?}", i, error);
+            }
+        };
+        writer.append_ser(user)?;
+    }
+
+    Ok(())
+}
+
+fn lines<P: AsRef<Path>>(
+    path: P,
+) -> Result<Box<dyn Iterator<Item = Result<String, std::io::Error>>>, std::io::Error> {
+    let extension = path
+        .as_ref()
+        .extension()
+        .and_then(|extension| extension.to_str())
+        .map(|extension| extension.to_lowercase());
+    let file = File::open(path)?;
+
+    if extension == Some("gz".to_string()) {
+        Ok(Box::new(BufReader::new(GzDecoder::new(file)).lines()))
+    } else {
+        Ok(Box::new(BufReader::new(file).lines()))
+    }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -51,6 +85,8 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error("Avro error")]
     Avro(#[from] apache_avro::Error),
+    #[error("User profile Avro error")]
+    UserAvro(#[from] twprs::avro::Error),
     #[error("JSON error")]
     Json(#[from] serde_json::Error),
 }
@@ -74,5 +110,10 @@ enum Command {
         /// Output path
         #[clap(short, long)]
         output: String,
+    },
+    Dump {
+        /// Input path
+        #[clap(short, long)]
+        input: String,
     },
 }
